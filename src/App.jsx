3ad1349a7ac1f4cfc2c71ee7791b376d1ee7e3a5
DeepDhamala <139035009+DeepDhamala@users.noreply.github.com>
2023-10-03 22:10:14 +0545

    Update App.jsx 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Container } from '@mui/material'
 import './App.css'
 import ResponsiveAppBar from './components/Header/Header'
-import Home from './pages/Home/Home'
+import Home from './pages/Home'
 import MessageFromPresident from './components/messagefrompresident/messagefrompresident'
 import Specialities from './components/specialities/Specialities'
 import Seeking from './components/seeking/Seeking'
